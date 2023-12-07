5e6143b99f7402fbfb76c9330d9dcd192c2456a9
Brycycle <44349959+Bryce-Davidson@users.noreply.github.com>
2023-12-06 16:28:01 -0800

    Better logs

--- index.py
+++ index.py
@@ -1,5 +1,4 @@
-from lib.data import save_data
-from gym.wrappers import FlattenObservation
+import pandas as pd
 import numpy as np
 import keras
 import time
@@ -9,14 +8,16 @@ from courses import tenByOneKm, flat, rollingHills, complicated
 from env import RiderEnv
 from lib.JsonEncoders import NpEncoder
 
-WEIGHTS_FOLDER_PATH = "./models/weights/"
-MODEL_WEIGHTS_FILE_NAME = "DMC-100m-tenByOneKm-06-12-2023_13:49.keras"
+WEIGHTS_FOLDER_PATH = "./models/weights"
+MODEL_SLUG = "DMC-100m-tenByOneKm-06-12-2023_13:49"
 
 # load the model from the .keras file
-model = keras.models.load_model(f"{WEIGHTS_FOLDER_PATH}/{MODEL_WEIGHTS_FILE_NAME}")
+model = keras.models.load_model(f"{WEIGHTS_FOLDER_PATH}/{MODEL_SLUG}.keras")
+
+FORCED_ACTION = 10
 
 if __name__ == "__main__":
-    env = RiderEnv(gradient=tenByOneKm, distance=100)
+    env = RiderEnv(gradient=tenByOneKm, distance=10_000)
 
     data = []
 
@@ -25,26 +26,29 @@ if __name__ == "__main__":
     while env.cur_position < env.COURSE_DISTANCE:
         state = np.array([state])
 
-        action_values = model.predict(state, verbose=0)[0]
-        action = np.argmax(action_values)
-
-        # action = 10
+        if FORCED_ACTION:
+            action = FORCED_ACTION
+        else:
+            action = np.argmax(model.predict(state, verbose=0)[0])
 
         state, reward, terminated, truncated, info = env.step(action)
 
+        data.append(info)
+
         print(
             json.dumps(
-                {
-                    **info,
-                    # "all_action_values": action_values,
-                    "predicted_reward": action_values[action],
-                },
+                info,
                 indent=4,
                 cls=NpEncoder,
             )
         )
 
-        data.append(info)
-        time.sleep(0.11)
+        time.sleep(0.1)
 
-        save_data("100_rider", data)
+    # Save the data to a csv file
+
+    df = pd.DataFrame(data)
+    if FORCED_ACTION:
+        df.to_csv(f"./models/forced_action_{FORCED_ACTION}.csv")
+    else:
+        df.to_csv(f"./models/{MODEL_SLUG}.csv")
--- lib/data.py
+++
@@ -1,7 +0,0 @@
-from typing import List
-import pandas as pd
-
-
-def save_data(name: str, data: List[dict]) -> None:
-    df = pd.DataFrame(data)
-    df.to_csv(f"data/{name}.csv", index=False)
