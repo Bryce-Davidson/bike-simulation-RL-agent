a4ce05451e14a014a1aef21985b8050c682d3d96
Brycycle <44349959+Bryce-Davidson@users.noreply.github.com>
2023-12-07 14:54:59 -0800

    Updated courses for test only

--- courses.py
+++ courses.py
@@ -1,44 +1,6 @@
 import numpy as np
 
 
-def tenByOneKm(x):
-    return np.piecewise(
-        x,
-        [
-            np.logical_and(0 <= x, x <= 1000),
-            np.logical_and(1000 < x, x <= 2000),
-            np.logical_and(2000 < x, x <= 3000),
-            np.logical_and(3000 < x, x <= 4000),
-            np.logical_and(4000 < x, x <= 5000),
-            np.logical_and(5000 < x, x <= 6000),
-            np.logical_and(6000 < x, x <= 7000),
-            np.logical_and(7000 < x, x <= 8000),
-            np.logical_and(8000 < x, x <= 9000),
-            np.logical_and(9000 < x, x <= 10000),
-        ],
-        [
-            lambda _: 15,
-            lambda _: -15,
-            lambda _: 15,
-            lambda _: -15,
-            lambda _: 15,
-            lambda _: -15,
-            lambda _: 15,
-            lambda _: -15,
-            lambda _: 15,
-            lambda _: -15,
-        ],
-    )
-
-
-def flat(x):
-    return 0
-
-
-def rollingHills(x):
-    return np.cos(x / 200) * 100
-
-
 def testCourse(x):
     if x <= 100:
         return 15
